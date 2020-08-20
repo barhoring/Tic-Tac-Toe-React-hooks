fd74dbe67090a1c6ab83c0c5ea0d9e07ecd22ea1
Bar Horing Amir <bar.apple@gmail.com>
2020-08-20 19:30:43 +0300

    save

---
+++ precache-manifest.c55151447e9349b212af056e7fd69c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef77fb383aad96a0b0523352159b110d",
+    "url": "/Tic-Tac-Toe-React-hooks/index.html"
+  },
+  {
+    "revision": "d0c30dde9e9d79b89bce",
+    "url": "/Tic-Tac-Toe-React-hooks/static/css/main.7712a8b8.chunk.css"
+  },
+  {
+    "revision": "a92abe992c37518c069b",
+    "url": "/Tic-Tac-Toe-React-hooks/static/js/2.bd9ef5e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React-hooks/static/js/2.bd9ef5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c30dde9e9d79b89bce",
+    "url": "/Tic-Tac-Toe-React-hooks/static/js/main.ba6b18ea.chunk.js"
+  },
+  {
+    "revision": "775aea616849fc0d6180",
+    "url": "/Tic-Tac-Toe-React-hooks/static/js/runtime-main.4d4f724e.js"
+  }
+]);
